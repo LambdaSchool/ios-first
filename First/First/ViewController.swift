a660e461eb272e51fd60be0bca86bb7a1fa978a6
Erica Sadun <erica@ericasadun.com>
2018-11-05 11:50:20 -0700

    add tap counter

--- First/First/ViewController.swift
+++ First/First/ViewController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
     
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func didTap(_ sender: Any) {
+        print("Tapped")
+        count += 1
+        label.text = "\(count)" // String(count)
+    }
 }
